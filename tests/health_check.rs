95fde1cbfdaec951bcd72cdfd7f869f3c2b4b242
TheRealSeber <rydzseba@gmail.com>
2024-04-02 01:08:17 +0200

    feat: chapter 5

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,7 @@
 use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
+use std::vec;
 use uuid::Uuid;
 
 use zero2prod::configurations::{load_configuration, DatabaseSettings};
@@ -82,7 +83,7 @@ async fn health_check_works() {
 }
 
 #[tokio::test]
-async fn subsribe_returns_a_200_for_valid_form_data() {
+async fn subscribe_returns_a_200_for_valid_form_data() {
     let app = spawn_app().await;
 
     let client = reqwest::Client::new();
@@ -132,6 +133,34 @@ async fn subscribe_return_a_400_when_data_is_missing() {
             response.status().as_u16(),
             "The API did not failt with 400 Bad Request when payload was {}.",
             error_message
+        );
+    }
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
+    ];
+
+    for (body, description) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not return a 400 OK when the payload was {}.",
+            description 
         );
     }
 }
